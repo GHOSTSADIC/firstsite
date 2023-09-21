dd2a5f6826975cda8255018ecf927a4c83869c20
GHOSTSADIC <dioufibrahim28@icloud.com>
2023-09-21 12:38:50 +0000

    nice

---
+++ main/main.js
@@ -0,0 +1,5 @@
+let openShoppping = document.querySelector(".cart-plus")
+let closeShopping = document.querySelector(".cart-plus")
+let plusCardShopping = document.querySelector(".circle-plus")
+let addToCard = document.querySelector(".ponclick")
+
